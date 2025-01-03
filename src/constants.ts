9726583967f41dc51cf65c491a75e4bfdf878629
Yash <yashskolambekar2015@gmail.com>
2025-01-03 15:14:31 +0530

    0.0.8 : fix #1 : changed source to r2 bucket

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-const bmpLogo = "https://files.20022005.xyz/bmp-blacked.png";
-const robotoNormal = "https://files.20022005.xyz/Roboto-Regular.ttf";
-const robotoBold = "https://files.20022005.xyz/Roboto-Bold.ttf";
+const bmpLogo = "https://r2.bookmypuja.org/bmp-blacked.png";
+const robotoNormal = "https://r2.bookmypuja.org/fonts/Roboto-Regular.ttf";
+const robotoBold = "https://r2.bookmypuja.org/fonts/Roboto-Bold.ttf";
 
 export { bmpLogo, robotoNormal, robotoBold };
