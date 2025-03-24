e20610994f3802137e57243c4b750e7c9951b8d6
Yash <yashskolambekar2015@gmail.com>
2025-03-24 17:32:54 +0530

    0.2.0: updated the assets urls

--- package/src/constants.ts
+++ package/src/constants.ts
@@ -1,9 +1,5 @@
 const bmpLogo = "https://res.cloudinary.com/dpaigt2bx/image/upload/f_auto,q_auto/v1/General%20BMP%20assets/vkesyodnuxuwevdwrk6m";
-// const robotoNormal = "https://r2.bookmypuja.org/fonts/Roboto-Regular.ttf";
-// const robotoBold = "https://r2.bookmypuja.org/fonts/Roboto-Bold.ttf";
-
-// const bmpLogo = "https://files.20022005.xyz/bmp-blacked.png";
-const robotoNormal = "https://files.20022005.xyz/Roboto-Regular.ttf";
-const robotoBold = "https://files.20022005.xyz/Roboto-Bold.ttf";
+const robotoNormal = "https://res.cloudinary.com/dpaigt2bx/raw/upload/v1742817493/fonts/ntbri6mclms8cfx3dk2e.ttf";
+const robotoBold = "https://res.cloudinary.com/dpaigt2bx/raw/upload/v1742817493/fonts/ojiymraiiuw7juzkmmu8.ttf";
 
 export { bmpLogo, robotoNormal, robotoBold };
