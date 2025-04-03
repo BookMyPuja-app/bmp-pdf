5bfdff6867e744a2ffea8deca0a3ac31cd618bec
Yash <yashskolambekar2015@gmail.com>
2025-04-03 14:14:30 +0530

    hotfix

--- package/src/sizes/a4/A4PrasadDelivery.tsx
+++ package/src/sizes/a4/A4PrasadDelivery.tsx
@@ -134,7 +134,7 @@ const A4PrasadDelivery = ({
                   }}
                 >
                   <Text>
-                    {item.date.toLocaleDateString("en-GB", {
+                    {new Date(item.date).toLocaleDateString("en-GB", {
                       day: "2-digit",
                       month: "2-digit",
                       year: "2-digit",
